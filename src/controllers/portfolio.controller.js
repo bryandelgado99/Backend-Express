7be274b2704944607759e506ab72f138af95a48b
Paul Delgado <bryand9970@gmail.com>
2023-12-19 16:27:10 -0500

    Portfolio Controller Script

--- src/controllers/portfolio.controller.js
+++ src/controllers/portfolio.controller.js
@@ -1,5 +1,6 @@
 const Portfolio = require('../models/Portfolio')
-const { uploadImage } = require('../config/cloudinary')
+const { uploadImage,deleteImage } = require('../config/cloudinary')
+const fs = require('fs-extra')
 
 //-------------------------------------------------------
 
@@ -28,6 +29,7 @@ const createNewPortafolio =async (req,res)=>{
         public_id:imageUpload.public_id,
         secure_url:imageUpload.secure_url
     }
+    await fs.unlink(req.files.image.tempFilePath)
     await newPortfolio.save()
     res.redirect('/portafolios')
 }
@@ -41,15 +43,35 @@ const renderEditPortafolioForm =async(req,res)=>{
 //-------------------------------------------------------
 const updatePortafolio = async(req,res)=>{
     const portfolio = await Portfolio.findById(req.params.id).lean()
-    if(!(portfolio.user.toString() !== req.user._id.toString())) return res.redirect('/portafolios')
-    const {title,category,description}= req.body
-    await Portfolio.findByIdAndUpdate(req.params.id,{title,category,description})
+    if(portfolio._id != req.params.id) return res.redirect('/portafolios')
+    
+    if(req.files?.image) {
+        if(!(req.files?.image)) return res.send("Se requiere una imagen")
+        await deleteImage(portfolio.image.public_id)
+        const imageUpload = await uploadImage(req.files.image.tempFilePath)
+        const data ={
+            title:req.body.title || portfolio.name,
+            category: req.body.category || portfolio.category,
+            description:req.body.description || portfolio.description,
+            image : {
+            public_id:imageUpload.public_id,
+            secure_url:imageUpload.secure_url
+            }
+        }
+        await fs.unlink(req.files.image.tempFilePath)
+        await Portfolio.findByIdAndUpdate(req.params.id,data)
+    }
+    else{
+        const {title,category,description}= req.body
+        await Portfolio.findByIdAndUpdate(req.params.id,{title,category,description})
+    }
     res.redirect('/portafolios')
 }
 
 //------------------------------------------------------
 const deletePortafolio = async(req,res)=>{
-    await Portfolio.findByIdAndDelete(req.params.id)
+    const portafolio = await Portfolio.findByIdAndDelete(req.params.id)
+    await deleteImage(portafolio.image.public_id)
     res.redirect('/portafolios')
 }
 
